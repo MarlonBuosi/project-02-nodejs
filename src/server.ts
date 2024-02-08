c8b24ce66b1b9010647a5111a335aad6f8c84fd1
MarlonBuosi <marlon.v.buosi@eexonmobil.com>
2024-02-07 21:02:02 -0300

    add default prot

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import { env } from './env'
 import { app } from './app'
 
+console.log(env.PORT)
+
 app.listen({
+  host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
   port: env.PORT,
 }).then(() => {
   console.log('HTTP server running')
